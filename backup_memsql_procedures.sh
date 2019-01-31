8f59cafaa42514e0d4829a99ae4488a5f0349377
mike czabator <mczabator@memsql.com>
2019-01-31 17:06:53 -0500

    updated

--- backup_memsql_procedures.sh
+++ backup_memsql_procedures.sh
@@ -3,7 +3,8 @@
    #                                                                                                                                              
    #         FILE: backup_memsql_procedures.sh [memsql connection string ex. -h127.0.0.1 -uroot -pPassword -P3306]
    #
-   #        USAGE: Run it
+   #        USAGE: 1) Run it. 
+   #               2) VERIFY ACCURACY OF EXPORT!
    #
    #  DESCRIPTION: Exports all MemSQL stored procedures 
    #      OPTIONS:  
@@ -12,12 +13,12 @@
    #      CREATED: 09.18.2018
    #      UPDATED: 01.31.2019      
    #      VERSION: 2.0
-   #      EUL    : 	THIS CODE IS OFFERED ON AN “AS-IS” BASIS AND NO WARRANTY, EITHER EXPRESSED OR IMPLIED, IS GIVEN. 
-   #				THE AUTHOR EXPRESSLY DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER EXPRESS OR IMPLIED.
-   #				YOU ASSUME ALL RISK ASSOCIATED WITH THE QUALITY, PERFORMANCE, INSTALLATION AND USE OF THE SOFTWARE INCLUDING, 
-   #				BUT NOT LIMITED TO, THE RISKS OF PROGRAM ERRORS, DAMAGE TO EQUIPMENT, LOSS OF DATA OR SOFTWARE PROGRAMS, 
-   #				OR UNAVAILABILITY OR INTERRUPTION OF OPERATIONS. 
-   #				YOU ARE SOLELY RESPONSIBLE FOR DETERMINING THE APPROPRIATENESS OF USE THE SOFTWARE AND ASSUME ALL RISKS ASSOCIATED WITH ITS USE.
+   #      EUL    :   THIS CODE IS OFFERED ON AN “AS-IS” BASIS AND NO WARRANTY, EITHER EXPRESSED OR IMPLIED, IS GIVEN. 
+   #           THE AUTHOR EXPRESSLY DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER EXPRESS OR IMPLIED.
+   #           YOU ASSUME ALL RISK ASSOCIATED WITH THE QUALITY, PERFORMANCE, INSTALLATION AND USE OF THE SOFTWARE INCLUDING, 
+   #           BUT NOT LIMITED TO, THE RISKS OF PROGRAM ERRORS, DAMAGE TO EQUIPMENT, LOSS OF DATA OR SOFTWARE PROGRAMS, 
+   #           OR UNAVAILABILITY OR INTERRUPTION OF OPERATIONS. 
+   #           YOU ARE SOLELY RESPONSIBLE FOR DETERMINING THE APPROPRIATENESS OF USE THE SOFTWARE AND ASSUME ALL RISKS ASSOCIATED WITH ITS USE.
    #
    #
    #
@@ -25,41 +26,57 @@
    #
    #
    #===============================================================================================================
+
+## set this for for singular procedure files, or database files:
+#  SINGLE   : single file for each procedure
+#  DB       : procedures in files for each database
+filetype=DB
+
 datetime=$(date +%Y%m%d_%H%M%S)
-dir=memsql_procedure_dump_$datetime	
+dir=memsql_procedure_dump_$datetime 
 
 for db in `memsql -N $@ -e "show databases"` 
 do
-   if [[ ! -e $dir ]]; then
+   if [[ ! -e $dir ]] ; then
     mkdir $dir
-   elif [[ ! -d $dir ]]; then
+   elif [[ ! -d $dir ]] ; then
        echo "$dir already exists but is not a directory" 1>&2
+       exit
    fi
 
-	if [ $db = "memsql" ] || [ $db = "cluster" ] ; then 
-		continue
-	else
+   if [[ $db = "memsql" ]] || [[ $db = "cluster" ]] ; then 
+      continue
+   else
       for sp_name in `memsql -N $@ $db -Bse "show procedures" | awk '{print $1}'`
       do
+         if [[ $filetype = "DB" ]] ; then
+            fn=$db
+         elif [[ $filetype = "SINGLE" ]]; then
+            fn=$db\.$sp_name
+         else  
+            echo "incorrect file type value!  use SINGLE or DB"
+            rmdir $dir
+            exit
+         fi
          printf "backing up $db.$sp_name\n"
-         printf "/*\ndatabase : $db\nprocedure: $sp_name\n*/\nDELIMITER //\n" >> ./$dir/$db\.sql
-         memsql $@ -D $db -ANe "show create procedure $sp_name\G" >  ./$dir/$db\.tmp
+         printf "/*\ndatabase : $db\nprocedure: $sp_name\nretrieved: $datetime\n*/\nDELIMITER //\n" >> ./$dir/$fn\.sql
+         memsql $@ -D $db -ANe "show create procedure $sp_name\G" >  ./$dir/$fn\.tmp
          
          # Remove Top 2 Lines
-         LINECOUNT=`wc -l < ./$dir/$db\.tmp`
+         LINECOUNT=`wc -l < ./$dir/$fn\.tmp`
          (( LINECOUNT -= 2 ))
-         tail -n ${LINECOUNT} < ./$dir/$db\.tmp > ./$dir/$db\.2.tmp
+         tail -n ${LINECOUNT} < ./$dir/$fn\.tmp > ./$dir/$fn\.2.tmp
 
          # Remove Bottom 2 Lines
-         LINECOUNT=`wc -l < ./$dir/$db\.2.tmp`
+         LINECOUNT=`wc -l < ./$dir/$fn\.2.tmp`
          (( LINECOUNT -= 2 ))
-         head -n ${LINECOUNT} < ./$dir/$db\.2.tmp >> ./$dir/$db\.sql
+         head -n ${LINECOUNT} < ./$dir/$fn\.2.tmp >> ./$dir/$fn\.sql
          
-         printf "//\nDELIMITER ;\n\n" >> ./$dir/$db\.sql
+         printf "//\nDELIMITER ;\n\n" >> ./$dir/$fn\.sql
          count=$(($count+1))
          rm ./$dir/*.tmp
-	done
-	fi
+   done
+   fi
 done
 
 if [[ $count > 0 ]]; then
