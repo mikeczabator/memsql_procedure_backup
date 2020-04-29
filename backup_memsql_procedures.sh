33dd3d493303ddd632c6040cf1212b36b793ec66
mike cz <35580719+mikeczabator@users.noreply.github.com>
2020-04-29 07:35:46 -0400

    Update backup_memsql_procedures.sh
    changed the way the perl regex looked for CREATE PROCEDURE to make sure it doesn't change other strings.
    MemSQL 7.0 added SQL_MODE to the SHOW PROCEDURE output.  added support to remove that line.

--- backup_memsql_procedures.sh
+++ backup_memsql_procedures.sh
@@ -11,9 +11,11 @@
    # REQUIREMENTS: MemSQL
    #       AUTHOR: Mike Czabator (mczabator@memsql.com)
    #      CREATED: 09.18.2018
-   #      UPDATED: 01.31.2019      
-   #      VERSION: 2.2
+   #      UPDATED: 04.29.2020      
+   #      VERSION: 2.3
    #    CHANGELOG: 2019-04-18  :  v2.2   : added perl regex to change CREATE PROCEDURE to CREATE OR REPLACE PROCEDURE
+   #               2020-04-29  :  v2.3   : changed the way the perl regex looked for CREATE PROCEDURE to make sure it doesn't change other strings.
+   #                                     : MemSQL 7.0 added SQL_MODE to the SHOW PROCEDURE output.  added support to remove that line.
    # 
    #      EUL    :   THIS CODE IS OFFERED ON AN “AS-IS” BASIS AND NO WARRANTY, EITHER EXPRESSED OR IMPLIED, IS GIVEN. 
    #           THE AUTHOR EXPRESSLY DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER EXPRESS OR IMPLIED.
@@ -37,9 +39,9 @@ function BACKUP_TYPE()
    if [ -z "$filetype" ];
    then 
       echo "Please set the backup type you would like."
-      echo "Enter SINGLE to backup procedures in single file for each database"
+      echo "Enter SINGLE to backup procedures in single file for each procedure"
       echo "-or-"
-      echo "enter DB to backup procedures in individual files"
+      echo "enter DB to backup procedures in a file per database"
       read filetype
       export filetype=$filetype
    fi
@@ -50,6 +52,9 @@ function BACKITUP()
 {
 datetime=$(date +%Y%m%d_%H%M%S)
 dir=memsql_procedure_dump_$datetime 
+
+memsql_version=`memsql -s -N -e "SELECT @@MEMSQL_VERSION"`
+echo MemSQL version $memsql_version found.
 
 for db in `memsql -N $@ -e "show databases"` 
 do
@@ -78,10 +83,27 @@ do
          printf "/*\ndatabase : $db\nprocedure: $sp_name\nretrieved: $datetime\n*/\nDELIMITER //\n" >> ./$dir/$fn\.sql
          memsql $@ -D $db -ANe "show create procedure $sp_name\G" 2>&1 | grep -v "Using a password on the command line interface can be insecure." >  ./$dir/$fn\.tmp
          
-         # Remove Top 2 Lines
-         LINECOUNT=`wc -l < ./$dir/$fn\.tmp`
-         (( LINECOUNT -= 2 ))
-         tail -n ${LINECOUNT} < ./$dir/$fn\.tmp > ./$dir/$fn\.2.tmp
+         if [ ${memsql_version:0:1} -le 6 ]
+         then 
+            # Remove Top 2 Lines for MemSQL version 6.8 and lower
+            LINECOUNT=`wc -l < ./$dir/$fn\.tmp`
+            (( LINECOUNT -= 2 ))
+            tail -n ${LINECOUNT} < ./$dir/$fn\.tmp > ./$dir/$fn\.2.tmp
+
+         elif [[ ${memsql_version:0:1} -ge 9 ]]; then
+            # Remove Top 3 Lines for MemSQL version 7.0 and higher
+            # MemSQL 7.0+ started adding the SQL_MODE to the SHOW CREATE PROCEDURE output, so we have to remove that from the output
+            LINECOUNT=`wc -l < ./$dir/$fn\.tmp`
+            (( LINECOUNT -= 3 ))
+            tail -n ${LINECOUNT} < ./$dir/$fn\.tmp > ./$dir/$fn\.2.tmp
+
+         else
+            printf "unsupported version of MemSQL found.  Test outputted procedures to make sure they work.\n"
+            #this could occur with betas.  remove top 3 lines assuming SQL_MODE will be there
+            LINECOUNT=`wc -l < ./$dir/$fn\.tmp`
+            (( LINECOUNT -= 3 ))
+            tail -n ${LINECOUNT} < ./$dir/$fn\.tmp > ./$dir/$fn\.2.tmp
+         fi
 
          # Remove Bottom 2 Lines
          LINECOUNT=`wc -l < ./$dir/$fn\.2.tmp`
